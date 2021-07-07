1d04df3f42a5af887fc652a92550e011fcba1c95
Divin Irakiza <irakizadivin@gmail.com>
2021-07-07 10:58:09 +0200

    Initial Commit

---
+++ src/main/java/com/java/main/springbootpgboilerplate/SpringBootPgBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.java.main.springbootpgboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPgBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPgBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/main/springbootpgboilerplate/SpringBootPgBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.main.springbootpgboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPgBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
